9f1b27dce7cbf4429a8976feb0362e27bf0ac80d
Julian Rosse <julian@helixbass.net>
2020-07-24 19:39:30 -0400

    addRouting()

--- src/App.tsx
+++ src/App.tsx
@@ -1,87 +1,37 @@
 import React, {FC} from 'react'
-import {flowMax, addDisplayName, addEffect, addStateHandlers} from 'ad-hok'
+import {flowMax, addDisplayName} from 'ad-hok'
 import gsap from 'gsap'
-import {suppressUnlessProp} from 'ad-hok-utils'
-import {Transition} from 'react-transition-group'
 
 import {makeStyles} from 'utils/style'
 import colors from 'utils/colors'
 import {DrawSVGPlugin} from 'utils/gsap/DrawSVGPlugin'
 import {addRefs} from 'utils/refs'
-import addLocation from 'utils/addLocation'
-import typedAs from 'utils/typedAs'
 import Shapes from 'components/Shapes'
 import {addRefsContextProvider} from 'utils/refsContext'
-import {PageName, addCurrentPageContextProvider} from 'utils/routing'
 import ButtonLink from 'components/ButtonLink'
 import Function from 'components/Function'
+import {addRouting} from 'utils/routing'
 
 gsap.registerPlugin(DrawSVGPlugin)
 
-const Content: FC = flowMax(
-  addDisplayName('Content'),
-  addStateHandlers(
-    {
-      currentDisplayedPage: null as PageName | null,
-      currentRoutedPage: null as PageName | null,
-    },
-    {
-      onPageChange: ({currentDisplayedPage}) => (pageName: PageName) => ({
-        currentRoutedPage: pageName,
-        currentDisplayedPage: currentDisplayedPage ?? pageName,
-      }),
-      onExited: ({currentRoutedPage}) => () => ({
-        currentDisplayedPage: currentRoutedPage,
-      }),
-    },
-  ),
-  addLocation,
-  // eslint-disable-next-line ad-hok/dependencies
-  addEffect(
-    ({location: {pathname}, onPageChange}) => () => {
-      const pageName = typedAs<PageName>(
-        pathname === '/function' ? 'function' : 'form',
-      )
-      onPageChange(pageName)
-    },
-    ['location.pathname'],
-  ),
-  suppressUnlessProp(['currentRoutedPage', 'currentDisplayedPage']),
-  addCurrentPageContextProvider,
-  ({currentRoutedPage, currentDisplayedPage, onExited}) => (
-    <div css={styles.contentContainer}>
-      <div css={styles.buttonsContainer}>
-        <ButtonLink toPage="form">Form</ButtonLink>
-        <ButtonLink toPage="function">Function</ButtonLink>
-      </div>
-      <div css={styles.mainContainer}>
-        <Shapes />
-        <Transition
-          in={
-            currentRoutedPage === 'function' &&
-            currentDisplayedPage === 'function'
-          }
-          addEndListener={() => {}}
-          timeout={2000}
-          onExited={() => onExited()}
-        >
-          {(state) => (
-            <>
-              {['entering', 'entered', 'exiting'].includes(state) && (
-                <Function hide={['exiting'].includes(state)} />
-              )}
-            </>
-          )}
-        </Transition>
-      </div>
+const Content: FC = flowMax(addDisplayName('Content'), () => (
+  <div css={styles.contentContainer}>
+    <div css={styles.buttonsContainer}>
+      <ButtonLink toPage="form">Form</ButtonLink>
+      <ButtonLink toPage="function">Function</ButtonLink>
     </div>
-  ),
-)
+    <div css={styles.mainContainer}>
+      <Shapes />
+      <Function />
+    </div>
+  </div>
+))
 
 const App: FC = flowMax(
   addDisplayName('App'),
   addRefs,
   addRefsContextProvider,
+  addRouting,
   () => (
     <>
       <link rel="stylesheet" href="https://use.typekit.net/wkh0yki.css" />
--- src/utils/routing.tsx
+++ src/utils/routing.tsx
@@ -1,5 +1,15 @@
+import {
+  flowMax,
+  addStateHandlers,
+  addEffect,
+  SimpleUnchangedProps,
+} from 'ad-hok'
+import {suppressUnlessProp, cleanupProps} from 'ad-hok-utils'
+
 import getContextHelpers from 'utils/getContextHelpers'
 import {toObjectKeys} from 'utils/fp'
+import addLocation from 'utils/addLocation'
+import typedAs from 'utils/typedAs'
 
 export type PageName = 'function' | 'form'
 
@@ -12,4 +22,44 @@ const [
   onExited: () => void
 }>(toObjectKeys(['currentRoutedPage', 'currentDisplayedPage', 'onExited']))
 
-export {addCurrentPageContextProvider, addCurrentPageContext}
+export {addCurrentPageContext}
+
+type AddRoutingType = SimpleUnchangedProps
+
+export const addRouting: AddRoutingType = flowMax(
+  addStateHandlers(
+    {
+      currentDisplayedPage: null as PageName | null,
+      currentRoutedPage: null as PageName | null,
+    },
+    {
+      onPageChange: ({currentDisplayedPage}) => (pageName: PageName) => ({
+        currentRoutedPage: pageName,
+        currentDisplayedPage: currentDisplayedPage ?? pageName,
+      }),
+      onExited: ({currentRoutedPage}) => () => ({
+        currentDisplayedPage: currentRoutedPage,
+      }),
+    },
+  ),
+  addLocation,
+  // eslint-disable-next-line ad-hok/dependencies
+  addEffect(
+    ({location: {pathname}, onPageChange}) => () => {
+      const pageName = typedAs<PageName>(
+        pathname === '/function' ? 'function' : 'form',
+      )
+      onPageChange(pageName)
+    },
+    ['location.pathname'],
+  ),
+  suppressUnlessProp(['currentRoutedPage', 'currentDisplayedPage']),
+  addCurrentPageContextProvider,
+  cleanupProps([
+    'currentDisplayedPage',
+    'currentRoutedPage',
+    'onPageChange',
+    'onExited',
+    'location',
+  ]),
+)
